afd61a6f8f0b4907868c4ca607d62de8f5e0ac16
jnewman <jnewman@gpslsolutions.com>
2015-09-22 09:02:31 +0100

    Refactored
    Made the code a bit cleaner ready for new features.

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+$( document ).ready(init);
+
+function init(){
+    Templates.loadTemplate( 'word-playlist','views/word-playlist.hbs' );
+    Templates.loadTemplate( 'word-scoreboard','views/word-scoreboard.hbs' );
+}
+
+function showPlaylist(){
+    Templates.loadTemplate( 'word-playlist','views/word-playlist.hbs' ).then( function(){
+        Wordlist.getWordlists().then( function( wordlists ){
+            var firstList = wordlists.lists[0];
+            return Wordlist.getWordlist( firstList.uri ).then( function(words){
+                return {
+                    info: firstList,
+                    words: words
+                }
+            });
+        }).then( function( wordlist ){
+            console.log("wordlist ",wordlist)
+            var wd = new WordDisplay( 'display','word-playlist' );
+            wd.draw( wordlist );
+        })
+    })
+}
+function showScoreboard(){
+    Templates.loadTemplate( 'word-scoreboard','views/word-scoreboard.hbs' ).then( function(){
+        Wordlist.getWordlists().then( function( wordlists ){
+            var firstList = wordlists.lists[0];
+            return Wordlist.getWordlist( firstList.uri ).then( function(words){
+                return {
+                    info: firstList,
+                    words: words
+                }
+            });
+        }).then( function( wordlist ){
+            //console.log("wordlist ",wordlist)
+            var wd = new WordDisplay( 'display','word-scoreboard' );
+            wd.draw( wordlist );
+        })
+    })
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -13,11 +13,20 @@ var Templates = (function() {
 		// your code goes here
 
 		instance.loadTemplate = function( name,uri ){
+			if( templates[name] ){
+				var dataPromise = $.Deferred();
+				//console.log("Prm",dataPromise);
+				dataPromise.resolve( templates[name] );
+				return dataPromise;
+			}
 			var store = R.partial( storeTemplate, name );
 			return $.get( uri ).then( store );
 		}
 
 		instance.runTemplate = function( name,data ){
+			// Need to change so run, loads and load stores a template object
+			// load becomes add...
+			console.log("Templates ", templates[ name ]);
 			return templates[name]( data );
 		}
 
